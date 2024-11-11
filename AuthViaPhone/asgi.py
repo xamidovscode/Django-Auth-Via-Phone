f4f7f512124b8c9cd66f599c4b8543c46672fc71
xamidovscode <xamidovcoder@gmail.com>
2024-11-11 17:55:42 +0500

    news

--- AuthViaPhone/asgi.py
+++ AuthViaPhone/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for DjangoAuthViaPhone project.
+ASGI config for AuthViaPhone project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoAuthViaPhone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AuthViaPhone.settings')
 
 application = get_asgi_application()
--- AuthViaPhone/wsgi.py
+++ AuthViaPhone/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DjangoAuthViaPhone project.
+WSGI config for AuthViaPhone project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoAuthViaPhone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AuthViaPhone.settings')
 
 application = get_wsgi_application()
