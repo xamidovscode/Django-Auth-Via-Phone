0ac98323470df9eb567e55a7b303ba9e9e1d3a6e
xamidovscode <xamidovcoder@gmail.com>
2024-11-12 20:54:07 +0500

    any news

--- settings/settings.py
+++ settings/settings.py
@@ -113,7 +113,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
+# Static file
+# s (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
