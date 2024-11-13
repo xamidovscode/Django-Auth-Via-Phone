a7871e71530ab88418d5f1fd809fe98912c94635
xamidovscode <xamidovcoder@gmail.com>
2024-11-13 18:27:13 +0500

    serializer

--- custom_auth/serializers.py
+++ custom_auth/serializers.py
@@ -16,7 +16,7 @@ class RegisterSerializer(serializers.Serializer):
 
     def create(self, validated_data):
         user = models.CustomUser.objects.filter(phone=validated_data['phone']).first()
-        if models.CustomUser.objects.filter(phone=validated_data['phone']).first():
+        if user:
             user.password = validated_data['password']
             user.save()
             instance = user
