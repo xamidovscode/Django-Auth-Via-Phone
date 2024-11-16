aa27d4cf1c1fcca28c6a4ba68abfc21f7a64d8ab
xamidovscode <xamidovcoder@gmail.com>
2024-11-16 19:34:19 +0500

    finished

--- settings/settings.py
+++ settings/settings.py
@@ -130,7 +130,7 @@ AUTH_USER_MODEL = 'custom_auth.CustomUser'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
